3cbf6e6bd3c4c1b99648affb0dc032e8b728f0ed
mhdgilangalfaiz <mhdgilangalfaiz@gmail.com>
2026-07-29 10:29:25 +0700

    add: migrate ans, ques, sub

--- database/migrations/2026_07_29_085542_create_questions_table.php
+++ database/migrations/2026_07_29_085542_create_questions_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             // foreign key untuk relasi table subject
             $table->foreignIdFor(Subject::class)->constrained()->cascadeOnDelete();
             $table->longText('payload');
+            $table->char('correct_answer');
             $table->text('description')->nullable();
             $table->boolean('is_active')->default(true);
             $table->softDeletes();
